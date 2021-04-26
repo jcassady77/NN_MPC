9b313942af82459dbc9039717e7de62e90b4b1b5
Johnathan Cassady <jcassady@JohnathsMacBook.fios-router.home>
2021-04-25 20:07:16 -0400

    making sure we up to date

--- NeuralNet.m
+++ NeuralNet.m
@@ -3,13 +3,15 @@ classdef NeuralNet
     %   Neural net seperated into multiple layers
     
     properties
-        weights = {{[]}};
+        weights;
             %3D array of weights
             %d1 Layer number
             %d2 Neuron number
             %d3 Weight corresponding to neurons in previous layer
-        biases = {{}};
-            %one bias for each neuron fn
+        layerLen;
+            %how long each layer is (must be same for all layers)
+        numLayers;
+            %how many layers there are
         
     end
     
@@ -17,32 +19,77 @@ classdef NeuralNet
         function obj = NeuralNet(layers,layerDimension)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
-            obj.weights = cell(1, layers);
-            for i=1:length(obj.weights)
-                obj.weights{i} = cell(layerDimension, 1);
-                for j=1:layerDimension
-                    %Random number from -1 to 1
-                    obj.weights{i}{j} = 2.*rand(1, layerDimension) -1;
+            obj.numLayers = layers;
+            obj.layerLen = layerDimension;
+            obj.weights = ones(layers, layerDimension, layerDimension);
+            
+            for i = 1:layers
+                for j = 1:layerDimension
+                    obj.weights(i, j, :) = 2.*rand(1, 1, layerDimension) -1;%fix this
                 end
             end
-            
-            obj.biases = cell(1, layers);
-            for i=1:length(obj.biases)
-                obj.biases{i} = cell(layerDimension, 1);
-            end
+
+            obj.weights
 
         end
-        function sig = sigmoid(
-%         
-%         function runNet
-%         end
-%         function calculateError
-%         end
-%         function outputArg = method1(obj,inputArg)
-%             %METHOD1 Summary of this method goes here
-%             %   Detailed explanation goes here
-%             outputArg = obj.Property1 + inputArg;
-%         end
+        
+
+         function out = runNet(obj, in)
+             currLayer = in;
+             newLayer = in;
+             for i = 1:obj.numLayers
+                 for j = 1:obj.layerLen
+                    newLayer(j) = sigmoid(dot(currLayer, squeeze(obj.weights(i, j, :))));
+                 end
+                 currLayer = newLayer;
+             end
+             
+             out = currLayer;
+         end
+         
+         function accuracy = trainNet(obj, trainingIns, trainingOuts, iterations)
+             %training ins/outs are 2d arrays of system inputs/outputs with
+             %each column identifying a new case
+             for iter = 1:iterations
+                %Training iterations
+                for trainingCase = 1:size(trainingIns, 1)
+                    layers = ones(obj.numLayers, obj.layerLen);
+                    currIn = trainingIns(trainingCase);
+                    currOut = trainingOuts(trainingCase);
+
+                    for i = 1:obj.numLayers
+                        for j = 1:obj.layerLen
+                            layers(i, j) = sigmoid(dot(currIn, squeeze(obj.weights(i, j, :))));
+                        end
+                        currIn = squeeze(layers(i, :));
+                    end
+                    response = currIn;
+                    
+                    
+                    %CALCULATE ERRORS
+                    finalError = response - currOut%error in outputs
+                    finalDelta = finalError *sigD(squeeze(layers(obj.numLayers, :)));%finds error in last layer
+                  
+                    %for the rest of the layers
+                    % error of previous layer =  delta dot wieghts
+                    % previous delta = previous error * sigd(prev weights)
+                    
+                    %UPDATE WEIGHTS
+                    %for each set of weights
+                    %weights -= dot(startlayer, finlayerdelta)
+                end
+             end
+         end
+         %add something to make the outputs the same length as
+                    %inputs/layers
+    end
+    methods(Static)
+         function sig = sigmoidFn(x)
+            sig = 1/(1+exp(-x));
+         end
+         function sigD = sigD(x)
+             sigD = x .*(1-x);
+         end
     end
 end
 
--- Python_code/nn_reference.py
+++ Python_code/nn_reference.py
@@ -10,45 +10,62 @@ def sigmoid(x):
 def sigmoid_output_to_derivative(output):
     return output*(1-output)
 
-# input dataset
-X = np.array([  [0,1],
-                [0,1],
-                [1,0],
-                [1,0] ])
 
-# output dataset           
-y = np.array([[0,0,1,1]]).T
+ins = np.loadtxt(open("inputStep1.csv", "rb"), delimiter=",", skiprows=1)
+outs = np.loadtxt(open("outputStep1.csv", "rb"), delimiter=",", skiprows=1)
+outs = outs.transpose()
+
+print("ins")
+#print((ins))
+print("outs")
+#print((outs.transpose()))
+
+# input dataset
+X = np.array(ins)
+#X = np.array([  [0,1],
+#                [0,1],
+#                [1,0],
+#                [1,0] ])
+
+# output dataset    
+y = np.array(outs)      
+#y = np.array([[0,1],[0,1],[0,1],[0,1]])
+
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-weights1 = 2*np.random.random((2,1)) - 1
-weights2 = 2*np.random.random((1,4)) - 1
+weights1 = 2*np.random.random((2,2)) - 1
+weights2 = 2*np.random.random((2,10)) - 1
 
 
-for iter in range(10000):
-    # forward propagation
-    layer_0 = X
-    layer_1 = sigmoid(np.dot(layer_0,weights1))
-    layer_2 = sigmoid(np.dot(layer_1,weights2))
-    # how much did we miss?
-    layer_2_error = layer_2 - y
-    
-    # multiply how much we missed by the
-    # slope of the sigmoid at the values in l1
-    layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
+for iter in range(10):
+    for n in range(98):
+        # forward propagation
+        layer_0 = X[n*10+0:n*10+10]
+        layer_1 = sigmoid(np.dot(layer_0,weights1))
+        layer_2 = sigmoid(np.dot(layer_1,weights2))
+        
+        # how much did we miss?
+        layer_2_error = layer_2 - y[n*10+0:n*10+10]
+        print('error')
+        print(np.average(layer_2_error))
+        # multiply how much we missed by the
+        # slope of the sigmoid at the values in l1
+        layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
 
-    layer_1_error = layer_2_delta.dot(weights2.T)
+        layer_1_error = layer_2_delta.dot(weights2.T)
 
-    layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+        layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
 
-    dWeights1 = np.dot(layer_0.T,layer_1_delta)
-    dWeights2 = np.dot(layer_1.T,layer_2_delta)
-    # update weights
-    weights1 -= dWeights1
-    weights2 -= dWeights2
+        dWeights1 = np.dot(layer_0.T,layer_1_delta)
+        dWeights2 = np.dot(layer_1.T,layer_2_delta)
+        # update weights
+        weights1 -= dWeights1
+        weights2 -= dWeights2
 
 print("Output After Training:")
-print(layer_1)
+
+
